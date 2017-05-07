27371cd10ecd8d1827a3da5a4304f49c42c64ea5
Ralf Anton Beier <ralf_beier@me.com>
2017-05-07 18:45:53 +0000

    Taken  over the RMSE calculation from EKF project.

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using namespace std;
+
 
 Tools::Tools() {}
 
@@ -11,8 +13,22 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    if (estimations.size() != ground_truth.size()
+        || estimations.size() == 0) {
+        cout << "Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+    for (int i = 0; i < estimations.size(); ++i) {
+        VectorXd e = estimations[i] - ground_truth[i];
+        e = e.array() * e.array();
+        rmse += e;
+    }
+    double f = 1.0 / estimations.size();
+    rmse = f * rmse;
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
