2b13c032fc6eb68af26267cfd31273c8b5c0674f
Ralf Anton Beier <ralf_beier@me.com>
2017-06-08 06:11:04 +0200

    Added Lidar Update
    With this update the simulator is now running on DataSet 1.
    
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -278,6 +278,32 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
     You'll also need to calculate the lidar NIS.
     */
+    // measurement matrix
+    MatrixXd H_ = MatrixXd(2, 5);
+    MatrixXd R_ = MatrixXd(2, 2);
+    /**
+  * Set the process and measurement noises
+*/
+    H_ << 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0;
+    //measurement covariance matrix - laser
+    R_ << 0.0225, 0,
+            0, 0.0225;
+
+    VectorXd z = meas_package.raw_measurements_;
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 /**
